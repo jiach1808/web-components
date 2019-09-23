95d26f9adaf2acf6f968099d47c28a7ac5a072c1
wjc <2396554126@qq.com>
2019-09-23 09:50:05 +0800

    html引入两个js文件

---
+++ src/my-button.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+  <button class="btn" @click="add">
+     购物车
+  </button>
+ </div> 
+</template>
+
+<script>
+
+export default {
+   
+}
+</script>
+
+<style>
+   .btn {
+    color:red;
+   }
+</style>
